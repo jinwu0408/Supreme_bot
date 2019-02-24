83899c994a7eee8a1e4e80ce77c146098d202629
jinwu0408 <gew013@ucsd.edu>
2019-02-23 21:21:45 -0800

    2nd Try

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import requests
 import datetime
 from bs4 import BeautifulSoup
 import urllib
-import urllib2
+#import urllib2
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
+"""
 url = 'https://www.supremenewyork.com'
 now = datetime.datetime.now()
 page = requests.get(url)
@@ -16,11 +17,12 @@ shop = soup.find('a', attrs = {'class':'shop_link'})
 shop_suffix = shop['href']
 shop_url = url + shop_suffix
 shop_data = urllib2.urlopen(shop_url)
-
-chrome_path = "C:\Users\jinwu\Downloads\chromedriver.exe"
-driver = webdriver.Chrome()
-driver.get('http://www.google.com/xhtml');
-#driver.get(driver,url)
-#print(shop_data)
+"""
+#chrome_path = "\usr\local\bin\chromedriver"
+chrome_options = Options()
+chrome_options.add_argument("__headless")
+chrome_options.add_argument('--no-sanbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver_new = webdriver.Chrome(chrome_options= chrome_options, executable_path=r'/usr/local/bin/chromedriver')
 
 
