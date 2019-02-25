931b846b6623f639c0ce1f0caf243d9c7456b5a1
jinwu0408 <gew013@ucsd.edu>
2019-02-25 00:53:21 -0800

    get WebDriver to work

--- main.py
+++ main.py
@@ -4,25 +4,23 @@ import requests
 import datetime
 from bs4 import BeautifulSoup
 import urllib
+import webbrowser
 #import urllib2
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-"""
+#from selenium.webdriver.common.keys import keys
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install()
+
 url = 'https://www.supremenewyork.com'
 now = datetime.datetime.now()
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+driver.get(url)
+
 shop = soup.find('a', attrs = {'class':'shop_link'})
 shop_suffix = shop['href']
 shop_url = url + shop_suffix
-shop_data = urllib2.urlopen(shop_url)
-"""
-#chrome_path = "\usr\local\bin\chromedriver"
-chrome_options = Options()
-chrome_options.add_argument("__headless")
-chrome_options.add_argument('--no-sanbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver_new = webdriver.Chrome(chrome_options= chrome_options, executable_path=r'/usr/local/bin/chromedriver')
+#shop_data = urllib2.urlopen(shop_url)
 
-
+webbrowser.open(shop_url)
