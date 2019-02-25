3d50a2fbfed09a05f29f9a3da469c04a5e307354
jinwu0408 <gew013@ucsd.edu>
2019-02-25 01:39:33 -0800

    autofill&click link

--- main.py
+++ main.py
@@ -7,20 +7,34 @@ import urllib
 import webbrowser
 #import urllib2
 from selenium import webdriver
-#from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-driver = webdriver.Chrome(ChromeDriverManager().install())
 
-url = 'https://www.supremenewyork.com'
+#set datetime
 now = datetime.datetime.now()
+url = 'https://www.supremenewyork.com'
+my_email = 'jinwuewrew@gmail.com'
+
+#BeautifulSoup.get
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+#webdriver.get
+driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(url)
+driver.find_element_by_link_text('shop').click()
 
+email = driver.find_element_by_id('email')
+
+email.send_keys(my_email)
+
+driver.find_element_by_name("commit").click()
+
+
+"""
 shop = soup.find('a', attrs = {'class':'shop_link'})
 shop_suffix = shop['href']
 shop_url = url + shop_suffix
 #shop_data = urllib2.urlopen(shop_url)
-
+"""
 #webbrowser.open(shop_url)
