0e1e691ef64048aabad52c088ad3c15eea70a7fd
jinwu0408 <gew013@ucsd.edu>
2019-02-25 18:32:01 -0800

    add select color feature

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from bs4 import BeautifulSoup
 from user import Users
 from profile import profile_name
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-category = "skate" #from config.py
-keyword = 'Fruit'
+category = "t-shirts" #from config.py
+keyword = 'Fronts'
+color = "Orange"
 user = Users(profile_name)
+
 
 #set datetime
 now = datetime.datetime.now()
@@ -20,16 +23,32 @@ category_url = 'https://www.supremenewyork.com/shop/all/{}'.format(category)
 
 #start the browser
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
 #into the category_url
 driver.get(category_url)
-#search keywork
-driver.find_element_by_partial_link_text(keyword).click()
+
+
+#search item with the keyword and color
+item_keyword_all_colors = driver.find_elements_by_partial_link_text(keyword)
+item_keyword_colors = driver.find_elements_by_partial_link_text(color)
+for each_color in item_keyword_all_colors:
+    for each_item in item_keyword_colors:
+        if each_color.get_attribute('href') == each_item.get_attribute('href'):
+            item_true_link = each_color.get_attribute('href')
+            partial_true_link = item_true_link[(len(item_true_link)-9):]
+            item_ele = driver.find_element_by_xpath("//a[contains(@href, '{}')]".format(partial_true_link)).click()
+            #print("partial_true_link is: {}".format(partial_true_link))
+            break
+        else:
+            pass
 driver.implicitly_wait(1) # seconds
+
 
 #add to cart
 driver.find_element_by_xpath("//input[@name='commit'][@class='button']").click()
 driver.implicitly_wait(1)
+
+
+#click checkout
 driver.find_element_by_xpath("//a[@class='button checkout']").click()
 
 #autofill checkout page
@@ -51,10 +70,11 @@ zip.send_keys(user.address[2])
 city.send_keys(user.address[3])
 card_number.send_keys(user.card[0])
 
-#driver.find_element_by_xpath("//ins[@name='order[terms]']").click()
-#atc = soup.find('a', attrs = {'class':'shop_link'})
-#atc = driver.find_element_by_xpath("htme/body/div/div/div[2]/div/form/fieldset[2]//input/@type='submit']")
-#atc = item.find_element_by_xpath("//input[@class='button']").click()
+
+#click process payment
+
+
+###########################################################################################
 
 
 """
@@ -67,10 +87,6 @@ category_item = soup.findAll('a', attrs = {'class':'name-link'})
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(category_url)
     driver.find_element_by_partial__text(keyword).click()
-"""
-
-
-"""
 #webdriver.get
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(url)
@@ -81,10 +97,7 @@ email = driver.find_element_by_id('email')
 email.send_keys(my_email)
 
 driver.find_element_by_name("commit").click()
-"""
-
-"""
-shop = soup.find('a', attrs = {'class':'shop_link'})
+op = soup.find('a', attrs = {'class':'shop_link'})
 shop_suffix = shop['href']
 shop_url = url + shop_suffix
 #shop_data = urllib2.urlopen(shop_url)
--- profile.py
+++ profile.py
@@ -7,7 +7,7 @@ profile_name = {
 
         'tel':"(666)666-6666",
 
-        'address':['24206 Springwood Dr.', '', '91765', 'Diamond Bar', 'state', 'country'],
+        'address':['666 sixsixsix Dr.', '', '91765', 'Diamond Bar', 'state', 'country'],
 
         'card': ['6666666666666666','exp.month','exp.year','666'],
 
